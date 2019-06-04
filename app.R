da09ea8b02699d27730c679ff56f8ef09ea2327c
irv-irv <iangeles27@g.ucla.edu>
2019-06-04 13:49:28 -0700

    Shiny Script for Infectious Diesease Model

--- app.R
+++ app.R
@@ -1,86 +1,62 @@
-#Load Neccesary packages for script to run 
+
 library("deSolve")
 library("shiny")
-
-#Shiny Components
-#UserInput UI 
-ui <- fluidPage(
-  numericInput(inputId = "infectedpop",
-               label = "Number of People Infected at Begining",
-               min = 1,
-               max = 1000,
-               value = 1e-6
-            ),
+infectious <- function(t, y, p){
+  {S<- y[1] 
+  I<- y[2] 
+  R<- y[3] }
   
-  numericInput(inputId = "userbeta",
-               label = "Infection Rate",
-               min = 0,
-               max = 100,
-               value = 1.4247
-            ),
-  
-  numericInput(inputId = "usergamma",
-               label = "Recovery Rate",
-               min = 0,
-               max = 1,
-               value = 0.14286
-            ),
-  
-  numericInput(inputId = "timeframe",
-               label ="Number of Days",
-               min = 1,
-               max = 1000,
-               value = 70
-            ),
-  mainPanel(
-    plotOutput("matPlot")
-  
-  )
-)
-
-
-#Server 
-server <- function(input, output) {
-  dataInput <- reactive({
-    #Infectious Disease Mathematical Model aka SIR
-    sir <- function(time, state, parameters) {
-      
-      with(as.list(c(state, parameters)), {
-        
-        dS <- -beta * S * I
-        dI <-  beta * S * I - gamma * I
-        dR <-                 gamma * I
-        
-        return(list(c(dS, dI, dR)))
-      })
-    }
-    init <- c(
-      S = 1-input$infectedpop, 
-      I = input$infectedpop, 
-      R = 0.0)
-    #Parameters beta and gamma
-    parameters <- c(
-      beta = input$userbeta,
-      gamma = input$usergamma) 
-    #Parameters beta and gamma
-    times <- seq(0, input$timeframe, by = 1)
-    #Solve using ode (General Solver for Ordinary Differential Equations)
-    out <- as.data.frame(ode(y = init, times = times, func = sir, parms = parameters))
-    #Delete time variable
-    out$time <- NULL
-    list(out = out, times = times) 
-  })
-  output$matPlot <- renderPlot({
-    matplot(dataInput()$times,dataInput()$out, 
-            type = "l", 
-            xlab = "Time in Days", 
-            ylab = "Susceptibles and Recovereds", main = "SIR Model", 
-            lwd = 1, lty = 1, bty = "l", col = 2:4)
-  legend(40, 0.7, c("Susceptible", "Infected Population", "Recovered"), pch = 1, col = 2:4) })
-  
-
+  with(as.list(p), {
+    dS.dt<--B*I*S 
+    dI.dt<-B*I*S-g*I 
+    dR.dt<-g*I
+    return(list(c(dS.dt, dI.dt, dR.dt)))}) 
   
 }
 
+#User Interfa
+ui <- fluidPage(
+  numericInput(inputId = "totalpop",
+               label = "Total Population",
+               min = 0,
+               max = 10000000000000000000000000000000000000000000000,
+               value = 10000),
+  numericInput(inputId = "infectedpop",
+               label = "Number of People Infected at Begining",
+               min = 0,
+               max = 100000,
+               value = 10),
+  numericInput(inputId = "beta",
+               label = "Infection Rate",
+               min = 0,
+               max = 10000000,
+               value = 0.01),
+  numericInput(inputId = "gamma",
+               label = "Recovery Rate",
+               min = 0,
+               max = 10000000,
+               value = 4),
+  numericInput(inputId = "timeframe",
+               label ="Number of Months",
+               min = 1,
+               max = 1000000000000000,
+               value = 3),
+  mainPanel(plotOutput("matPlot"))
+    )
 
+#Server
+server <- function(input, output){
+  datainput <- reactive({
+    I <- R <- input$infectedpop
+    S <- ((input$totalpop) - I - R)
+    parms<- c(B =input$beta, g =input$gamma)
+    months <- seq(0, input$timeframe, by = 0.01)
+    out <- data.frame(ode(c(S, I, R), months, infectious, parms))
+    list(months = months, out = out)
+    })
+  output$matPlot <- renderPlot({
+    matplot(datainput()$months,datainput()$out[, -1], type = "l", lty = 1:3, col = 1 )
+    legend("right", c("R", "I", "S"), lty = 3:1, col = 3:1, bty = "n")
+           })
+}
 shinyApp(ui = ui, server = server)
